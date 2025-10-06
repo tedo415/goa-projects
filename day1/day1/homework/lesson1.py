81c1fc6177101b4857ce07b4635219367892fc2d
twdo415 <tedogabidzashvili@gmail.com>
2025-10-06 22:39:48 +0400

    third commit

--- day1/day1/homework/lesson1.py
+++ day1/day1/homework/lesson1.py
@@ -1,13 +1,5 @@
 from turtle import *
 
-# print("tedo gabidzashvili")
-
-# print(5)    #integer int
-# print("5")    #string str
-
-# print(5 + int("5"))
-
-# print("tedo gabidzashvili aris" + "5" + "wlis")
 
 speed(600)
 width(7)
